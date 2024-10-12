bae671afa1dda628ba09a14adaf64ba337a663f7
Rhcandy <134164626+Rhcandy@users.noreply.github.com>
2024-10-12 08:19:16 +0300

    Update Get.java

--- Get.java
+++ Get.java
@@ -4,6 +4,6 @@ import java.lang.annotation.*;
 @Retention(RetentionPolicy.RUNTIME)
 @Target({ElementType.METHOD})
 
-public @interface Get {
+public @interface GET {
     String value() default "";
 }
